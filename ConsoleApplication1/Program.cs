6330513442b91c14fbfc39adc62088f8f7dcb56d
Yuan <Michael.Yuan@bhpbilliton.com>
2017-01-06 11:46:24 +0800

    'all'

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using MyClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I'm ConsoleApplication1 say: hello!");
+
+
+            ClassDemo classDemo = new ClassDemo();
+            classDemo.ClassDemoSay();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MyClassLibrary/ClassDemo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyClassLibrary
+{
+    public class ClassDemo
+    {
+        public void ClassDemoSay()
+        {
+            Console.WriteLine("I'm ClassDemo say: Hello!");
+        }
+    }
+}
